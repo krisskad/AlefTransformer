64ca9d9b108ffcebbea9826a1d7b08cc3f7a8b9b
krisskad <krishna.kadam@ongil.ai>
2024-01-11 13:49:19 +0530

    take image

--- Transformer/templates/AudiowithInputBox_001/processor.py
+++ Transformer/templates/AudiowithInputBox_001/processor.py
@@ -263,14 +263,20 @@ def create_mlo(input_json_data, input_other_jsons_data, exiting_hashcode):
         """
     )
 
-    img_src = input_other_jsons_data['INPUT_IMAGES_JSON_DATA'][input_json_data["pageData"]["args"]['background']["src"]]
+    if "mediaBoxData" in input_json_data["pageData"]["args"]:
+        img_src = input_other_jsons_data['INPUT_IMAGES_JSON_DATA'][
+            input_json_data["pageData"]["args"]['mediaBoxData']["src"]]
+    else:
+        img_src = input_other_jsons_data['INPUT_IMAGES_JSON_DATA'][
+            input_json_data["pageData"]["args"]['background']["src"]]
+
     resp = copy_to_hashcode_dir(src_path=img_src, exiting_hashcode=exiting_hashcode)
     all_files.add(resp['relative_path'])
     exiting_hashcode.add(resp['hashcode'])
 
     all_tags.append(
         f"""
-            </alef_column>
+        </alef_column>
             <alef_column xlink:label="{temp[23]}" xp:name="alef_column" xp:description=""
                          xp:fieldtype="folder" width="auto" cellspan="1">
                 <alef_image xlink:label="{resp['hashcode']}" xp:name="alef_image" xp:description=""
