24961ff1df3a6402dc68275b11d7521fc96ec144
krisskad <krishna.kadam@ongil.ai>
2024-01-11 14:54:38 +0530

    alef

--- core/settings.py
+++ core/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'Transformer.custom_middleware.CustomMiddleware',
+    'Transformer.alef.CustomMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
